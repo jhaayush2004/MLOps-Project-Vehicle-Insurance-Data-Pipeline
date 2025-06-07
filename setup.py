4484bd5fcfc3bae20378df4fa03fad516a37d557
jhaayush2004 <shauryasphinx@gmail.com>
2025-06-07 21:16:07 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ayush Shaurya Jha",
+    author_email="shauryasphinx@gmail.com",
+    packages=find_packages()
+)
